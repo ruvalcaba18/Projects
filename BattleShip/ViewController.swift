834bdcd6c7f00c526c6ccc1f51998ce63d5ea589
jael <jael.r>
2019-11-18 10:21:19 -0800

    Initial Commit

---
+++ BattleShip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BattleShip
+//
+//  Created by MacOs on 18/11/19.
+//  Copyright © 2019 MacOs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
